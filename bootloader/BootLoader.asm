2383b42b9e2e4fe562297278efce439d36b3ac60
Seok-Hong <seok85.hong@gmail.com>
2014-10-19 13:36:59 +0900

    example 4-5 done.
    add a example that print 'TEN OS Bootloader Start~!!' message using additional data.
    and, add the clear whole screen logic.
    and, add the print message logic.

--- bootloader/BootLoader.asm
+++ bootloader/BootLoader.asm
@@ -7,22 +7,58 @@
 ; Changelog:
 ;	0.0.1 - first version
 ;------------------------------------------------------------------------------
-[ORG 0x00]	; set startup address to 0x00
-[BITS 16]	; use 16bit code
+[ORG 0x00]		; set startup address to 0x00
+[BITS 16]		; use 16bit code
 
-SECTION .text	; define .text section
+SECTION .text		; define .text section
 
-mov ax, 0xB800	; AX <- 0xB800
-mov ds, ax	; DS <- AX(0xB800)
+jmp 0x07c0:START	; far-jump to START label with store the 0x07C0 into CS
 
-mov byte [0x00], 'M'	; DS:offset; 0xB800:0x000 <- 'M'
-mov byte [0x01], 0x4A	; DS:offset; 0xB000:0x001 <- 0x4A 
-			; 0x4A; (background:red, foreground:light green)
+START:
+	mov ax, 0x07c0		; AX <- 0x07c0
+	mov ds, ax		; DS <- AX(0x07c0)
 
-jmp $		; run infinite-loop
+	mov ax, 0xB800		; AX <- 0xB800
+	mov es, ax		; ES <- AX(0xB800)
+
+	mov si, 0
+
+.SCREENCLEARLOOP:			; remove screen (80*28*2)
+	mov byte [es:si], 0		; remove character
+	mov byte [es:si + 1], 0x02	; see. table 4-2, on book1 (p.123)
+					; set background:black 
+					; set foreground:green
+
+	add si, 2		; move next character position
+
+	cmp si, 80*25*2		; si value will be size of screen
+
+	jl .SCREENCLEARLOOP	; if si less than 80*25*2, clear next position.
+
+	mov si, 0		; clear si register to print boot message
+	mov di, 0		; clear di register to print boot message
+
+.MESSAGELOOP:
+	mov cl, byte [si+MESSAGE1]	; set cl register to MESSAGE1 label
+
+	cmp cl, 0		; until cl == 0 (this mean, boot message is end with 0)
+	je .MESSAGEEND
+
+	mov byte [es:di], cl	; set character from MESSAGE1 label.
+
+	add si, 1		; move next character of MESSAGE1
+	add di, 2		; skip back/foreground character
+
+	jmp .MESSAGELOOP
+
+.MESSAGEEND:			; If we print all of MESSAGE1, we will be here.
+
+	jmp $			; run infinite-loop
+
+MESSAGE1:	db 'TEN OS Boot Loader Start~!!', 0	; BootLoader Message
 
 times 510 - ( $ - $$)	db 0x00	; $: current address
 				; $$: start address of currect section(.text section)
 				; $-$$: offset related by current section
 
-dw 0xAA55	; bootloader magic number
+dw 0xAA55		; bootloader magic number
