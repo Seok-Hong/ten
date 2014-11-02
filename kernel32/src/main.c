fd7d748488939ac3e626e495592db83bfb1e09d0
Seok Hong <seok85.hong@gmail.com>
2014-11-02 23:51:08 +0900

    change code formatter using linux's rule

--- kernel32/src/main.c
+++ kernel32/src/main.c
@@ -1,6 +1,6 @@
 #include "types.h"
 
-void kprint_string(int x, int y, const char* str);
+void kprint_string(int x, int y, const char *str);
 
 void main()
 {
@@ -9,16 +9,16 @@ void main()
 	while (1);
 }
 
-void kprint_string(int x, int y, const char* str)
-{        
+void kprint_string(int x, int y, const char *str)
+{
 	int i;
-        struct video_char_data* video_mem_addr = (struct video_char_data*)0xB8000;
+	struct video_char_data *video_mem_addr = (struct video_char_data *)0xB8000;
 
-        //calculate the position to print string
+	//calculate the position to print string
 	video_mem_addr = video_mem_addr + (y * 80) + x;
 
 	//print string
 	for (i = 0; str[i] != '\0'; i++) {
-                video_mem_addr[i].data = str[i];
+		video_mem_addr[i].data = str[i];
 	}
 }
